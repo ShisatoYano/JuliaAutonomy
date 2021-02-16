1b8a0403eb2e84343dcbafffd1a78fecec4a88d7
Shisato <shisatoyano@gmail.com>
2021-02-16 21:59:49 +0900

    adde test of draw histogram

--- test/test_sensor_data.jl
+++ test/test_sensor_data.jl
@@ -5,12 +5,16 @@ module TestSensorData
 
     # target modules
     include(joinpath(split(@__FILE__, "test")[1], "src/sensor_data/print_sensor_data.jl"))
+    include(joinpath(split(@__FILE__, "test")[1], "src/sensor_data/draw_histogram.jl"))
 
     function main()
         @testset "SensorData" begin
             @testset "PrintSensorData" begin
                 @test_nowarn PrintSensorData.print_data_200()
                 @test_nowarn PrintSensorData.print_data_600()
+            end
+            @testset "DrawHistogram" begin
+                @test_nowarn DrawHistogram.main()
             end
         end
     end
