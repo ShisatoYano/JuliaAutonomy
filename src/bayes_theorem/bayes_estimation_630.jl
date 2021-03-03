3f2353a8ce14ba8e22eec16c4acfbd4c27ffa144
Shisato <shisatoyano@gmail.com>
2021-03-03 14:31:41 +0000

    added bayes_estimation_630

---
+++ src/bayes_theorem/bayes_estimation_630.jl
@@ -0,0 +1,13 @@
+# estimate observed time by bayes theorem
+
+module BayseEst630
+    using DataFrames, CSV, FreqTables, Plots
+
+    function bayes_estimation()
+        
+    end
+
+    function main()
+        
+    end
+end
