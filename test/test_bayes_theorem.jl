f77a378d0cccac0c387f1ad134e9ed9da842b94d
Shisato <shisatoyano@gmail.com>
2021-03-05 12:34:25 +0000

    adde bayes_est_at_5 and 11

--- test/test_bayes_theorem.jl
+++ test/test_bayes_theorem.jl
@@ -6,6 +6,8 @@ module TestBayesTheorem
     # target modules
     include(joinpath(split(@__FILE__, "test")[1], "src/bayes_theorem/probs_bayes.jl"))
     include(joinpath(split(@__FILE__, "test")[1], "src/bayes_theorem/bayes_estimation_630.jl"))
+    include(joinpath(split(@__FILE__, "test")[1], "src/bayes_theorem/bayes_est_at_5.jl"))
+    include(joinpath(split(@__FILE__, "test")[1], "src/bayes_theorem/bayes_est_at_11.jl"))
 
     function main()
         @testset "BayesTheorem" begin
@@ -14,6 +16,12 @@ module TestBayesTheorem
             end
             @testset "BayesEst630" begin
                 @test BayesEst630.main() == true
+            end
+            @testset "BayesEstAt5" begin
+                @test BayesEstAt5.main() == true
+            end
+            @testset "BayesEstAt11" begin
+                @test BayesEstAt11.main() == true
             end
         end
     end
