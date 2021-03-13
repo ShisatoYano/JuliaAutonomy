d6cbe7c769f0a43f2c48c16c1b32b6a6e5bf64bf
Shisato <shisatoyano@gmail.com>
2021-03-13 12:46:37 +0000

    added complex_dist folder

--- test/test_prob_stats.jl
+++ test/test_prob_stats.jl
@@ -16,15 +16,14 @@ module TestProbStats
     include(joinpath(split(@__FILE__, "test")[1], "src/prob_stats/prob_model/gauss_prob_dist_cdf.jl"))
     include(joinpath(split(@__FILE__, "test")[1], "src/prob_stats/prob_model/bar_prob_dist_cdf.jl"))
     include(joinpath(split(@__FILE__, "test")[1], "src/prob_stats/prob_model/calc_expected_value.jl"))
-    include(joinpath(split(@__FILE__, "test")[1], "src/sensor_data/draw_hist_600.jl"))
-    include(joinpath(split(@__FILE__, "test")[1], "src/sensor_data/draw_time_series_600.jl"))
-    include(joinpath(split(@__FILE__, "test")[1], "src/sensor_data/group_by_hour.jl"))
-    include(joinpath(split(@__FILE__, "test")[1], "src/sensor_data/hist_grp_by_hour.jl"))
-    include(joinpath(split(@__FILE__, "test")[1], "src/sensor_data/group_hour_heatmap.jl"))
-    include(joinpath(split(@__FILE__, "test")[1], "src/sensor_data/marginal_kde.jl"))
-    include(joinpath(split(@__FILE__, "test")[1], "src/sensor_data/prob_sum_hour.jl"))
-    include(joinpath(split(@__FILE__, "test")[1], "src/sensor_data/prob_sum_lidar.jl"))
-    include(joinpath(split(@__FILE__, "test")[1], "src/sensor_data/cond_z_t_bar.jl"))
+    include(joinpath(split(@__FILE__, "test")[1], "src/prob_stats/complex_dist/draw_hist_600.jl"))
+    include(joinpath(split(@__FILE__, "test")[1], "src/prob_stats/complex_dist/draw_time_series_600.jl"))
+    include(joinpath(split(@__FILE__, "test")[1], "src/prob_stats/complex_dist/group_by_hour.jl"))
+    include(joinpath(split(@__FILE__, "test")[1], "src/prob_stats/complex_dist/hist_grp_by_hour.jl"))
+    include(joinpath(split(@__FILE__, "test")[1], "src/prob_stats/complex_dist/group_hour_heatmap.jl"))
+    include(joinpath(split(@__FILE__, "test")[1], "src/prob_stats/complex_dist/prob_sum_hour.jl"))
+    include(joinpath(split(@__FILE__, "test")[1], "src/prob_stats/complex_dist/prob_sum_lidar.jl"))
+    include(joinpath(split(@__FILE__, "test")[1], "src/prob_stats/complex_dist/cond_z_t_bar.jl"))
 
     function main()
         @testset "ProbStats" begin
@@ -67,32 +66,31 @@ module TestProbStats
                     @test_nowarn CalcExpValue.main()
                 end    
             end
-            @testset "DrawHist600" begin
-                @test DrawHist600.main() == true
-            end
-            @testset "DrawTimeSeries600" begin
-                @test DrawTimeSeries600.main() == true
-            end
-            @testset "GroupByHour" begin
-                @test GroupByHour.main() == true
-            end
-            @testset "HistGrpByHour" begin
-                @test HistGrpByHour.main() == true
-            end
-            @testset "GrpHrHeatmap" begin
-                @test GrpHrHeatmap.main() == true
-            end
-            # @testset "MarginalKde" begin
-            #     @test MarginalKde.main() == true
-            # end
-            @testset "ProbSumHour" begin
-                @test ProbSumHour.main() == 1.0
-            end
-            @testset "ProbSumLidar" begin
-                @test ProbSumLidar.main() == 1.0
-            end
-            @testset "CondZtBar" begin
-                @test CondZtBar.main() == true
+            @testset "ComplexDist" begin
+                @testset "DrawHist600" begin
+                    @test DrawHist600.main() == true
+                end
+                @testset "DrawTimeSeries600" begin
+                    @test DrawTimeSeries600.main() == true
+                end
+                @testset "GroupByHour" begin
+                    @test GroupByHour.main() == true
+                end
+                @testset "HistGrpByHour" begin
+                    @test HistGrpByHour.main() == true
+                end
+                @testset "GrpHrHeatmap" begin
+                    @test GrpHrHeatmap.main() == true
+                end
+                @testset "ProbSumHour" begin
+                    @test ProbSumHour.main() == 1.0
+                end
+                @testset "ProbSumLidar" begin
+                    @test ProbSumLidar.main() == 1.0
+                end
+                @testset "CondZtBar" begin
+                    @test CondZtBar.main() == true
+                end    
             end
         end
     end
