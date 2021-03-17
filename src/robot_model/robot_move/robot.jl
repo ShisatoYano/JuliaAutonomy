ca85a6c31ce9c2b3b827fa55ca9175e04f86ba62
Shisato <shisatoyano@gmail.com>
2021-03-17 13:23:48 +0000

    implemented world class

--- src/robot_model/robot_move/robot.jl
+++ src/robot_model/robot_move/robot.jl
@@ -24,10 +24,12 @@ function circle(x, y, r)
     x .+ r * sin.(theta), y .+ r * cos.(theta)
 end
 
-function draw(self::Robot)
+function draw!(self::Robot)
     x, y, theta = self.pose[1], self.pose[2], self.pose[3]
     xn = x + self.radius * cos(theta)
     yn = y + self.radius * sin(theta)
-    plot([x, xn], [y, yn], color=self.color, legend=false, aspect_ratio=true)
-    plot!(circle(x, y, self.radius), linecolor=self.color, aspect_ratio=true)
+    plot!([x, xn], [y, yn], color=self.color, 
+         legend=false, aspect_ratio=true)
+    plot!(circle(x, y, self.radius), linecolor=self.color, 
+          aspect_ratio=true)
 end
--- src/robot_model/robot_move/world.jl
+++ src/robot_model/robot_move/world.jl
@@ -1,19 +1,30 @@
-# define world coordinate system
+# class for defining world coordinate system
 
-module World
-    using Plots
-    pyplot()
+using Plots
+pyplot()
 
-    # objects array
-    objects = []
+mutable struct World
+    x_min
+    x_max
+    y_min
+    y_max
 
-    # register object
-    function append(obj)
-        push!(objects, obj)
+    # init
+    function World(x_min::Float64, x_max::Float64,
+                   y_min::Float64, y_max::Float64)
+        self = new()
+        self.x_min = x_min
+        self.x_max = x_max
+        self.y_min = y_min
+        self.y_max = y_max
+        return self
     end
+end
 
-    # draw registered objects
-    function draw()
-        
-    end
+function draw(self::World)
+    plot([], [], aspect_ratio=true,
+         xlabel="X", ylabel="Y",
+         xlims=(self.x_min, self.x_max),
+         ylims=(self.y_min, self.y_max),
+         legend=false)
 end
