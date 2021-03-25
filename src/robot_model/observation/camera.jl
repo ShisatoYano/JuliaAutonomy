7574b3279c075dec1f4ec91fc00edb4c4e92eb36
Shisato <shisatoyano@gmail.com>
2021-03-25 15:05:50 +0000

    implemented observation function

---
+++ src/robot_model/observation/camera.jl
@@ -0,0 +1,37 @@
+# class for camera observing landmark
+
+using Plots
+pyplot()
+
+include(joinpath(split(@__FILE__, "src")[1], "src/robot_model/observation/map.jl"))
+
+mutable struct Camera
+    map
+
+    # init
+    function Camera(map::Map)
+        self = new()
+        self.map = map
+        return self
+    end
+end
+
+function observation_function(cam_pose::Array, obj_pos::Array)
+    diff_x = obj_pos[1] - cam_pose[1]
+    diff_y = obj_pos[2] - cam_pose[2]
+    phi = atan(diff_y, diff_x) - cam_pose[3]
+    while phi >= pi
+        phi -= 2 * pi
+    end
+    while phi < -pi
+        phi += 2 * pi
+    end
+    return [hypot(diff_x, diff_y), phi]
+end
+
+function data(self::Camera, cam_pose::Array)
+    observed = []
+    for lm in self.map.landmarks
+        
+    end
+end
