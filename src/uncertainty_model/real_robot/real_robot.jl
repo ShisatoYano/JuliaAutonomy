62444639fd0b7c78ccb4b8f604e6f65bbdff54e7
Shisato <shisatoyano@gmail.com>
2021-03-30 12:50:26 +0000

    added agent class into real_robot class as member

--- src/uncertainty_model/real_robot/real_robot.jl
+++ src/uncertainty_model/real_robot/real_robot.jl
@@ -5,21 +5,28 @@
 using Plots
 pyplot()
 
+include(joinpath(split(@__FILE__, "src")[1], "src/robot_model/movement/agent.jl"))
+
 mutable struct RealRobot
     pose
     radius
     color
+    agent
+    delta_time
     traj_x
     traj_y
 
     # init
-    function RealRobot(pose::Array, radius::Float64, color::String)
+    function RealRobot(pose::Array, radius::Float64, color::String,
+                       agent::Agent, delta_time::Float64)
         self = new()
         self.pose = pose
         self.radius = radius
         self.color = color
-        self.traj_x = []
-        self.traj_y = []
+        self.agent = agent
+        self.delta_time = delta_time
+        self.traj_x = [pose[1]]
+        self.traj_y = [pose[2]]
         return self
     end
 end
@@ -45,15 +52,23 @@ function state_transition(speed, yaw_rate, time, pose)
 end
 
 function draw!(self::RealRobot)
+    # current pose
     x, y, theta = self.pose[1], self.pose[2], self.pose[3]
     xn = x + self.radius * cos(theta)
     yn = y + self.radius * sin(theta)
+    
+    # draw robot and current pose
     plot!([x, xn], [y, yn], color=self.color, 
          legend=false, aspect_ratio=true)
     plot!(circle(x, y, self.radius), linecolor=self.color, 
           aspect_ratio=true)
-    push!(self.traj_x, x)
-    push!(self.traj_y, y)
+    # draw trajectory
     plot!(self.traj_x, self.traj_y, color=self.color, 
           legend=false, aspect_ratio=true)
+    
+    # next pose
+    spd, yr = decision(self.agent)
+    next_pose = state_transition(spd, yr, self.delta_time, self.pose)
+    self.pose = next_pose
+    push!(self.traj_x, self.pose[1]), push!(self.traj_y, self.pose[2])
 end
