0a00647157015355fad892b3c73cd8b5b9b63fa1
Shisato <shisatoyano@gmail.com>
2021-05-02 13:52:55 +0000

    implemented motion test forward bias

--- src/localization/particle_filter/motion_test_forward_bias.jl
+++ src/localization/particle_filter/motion_test_forward_bias.jl
@@ -1,9 +1,9 @@
 # module for calculating standard deviation parameters
 # robot moves for 4m at 0.1m/s on straight trajectory
-# including random noise
+# including random noise and bias
 # this is iterated for 100 times
 
-module MotionTestForward
+module MotionTestForwardBias
   using DataFrames, Statistics
 
   include(joinpath(split(@__FILE__, "src")[1], "src/robot_model/movement/world.jl"))
@@ -22,7 +22,7 @@ module MotionTestForward
     for i in 1:100
       robot = RealRobot(initial_pose, 0.2, "black", straight, 
                         time_interval, noise_per_meter=5,
-                        noise_std=pi/60)
+                        noise_std=pi/60, bias_rate_stds=[0.1, 0.1])
       push!(robots, robot)  
     end
     
@@ -40,18 +40,18 @@ module MotionTestForward
       end
     end
 
-    save_path = joinpath(split(@__FILE__, "src")[1], "gif/motion_test_forward.gif")
+    save_path = joinpath(split(@__FILE__, "src")[1], "gif/motion_test_forward_bias.gif")
     gif(anim, fps=15, save_path)
 
     # calculate variance after moved
     poses = DataFrame(
                       range=[sqrt(r.pose[1]^2 + r.pose[2]^2) for r in robots],
                       theta=[r.pose[3] for r in robots])
-    theta_var = var(poses.theta, corrected=false)
+    range_var = var(poses.range, corrected=false)
     range_mean = mean(poses.range)
     println("Variance of range and theta: ", poses)
-    println("Variance of theta: ", theta_var)
+    println("Variance of range: ", range_var)
     println("Mean of range: ", range_mean)
-    println("Standard deviation per m of theta: ", sqrt(theta_var/range_mean))
+    println("Standard deviation per m of range: ", sqrt(range_var/range_mean))
   end
 end
