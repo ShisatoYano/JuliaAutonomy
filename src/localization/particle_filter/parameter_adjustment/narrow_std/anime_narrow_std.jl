1d358f63433559d3c5a89190fc146e1289321fef
Shisato <shisatoyano@gmail.com>
2021-05-08 08:55:49 +0000

    refactored narrow/wide_std modules

---
+++ src/localization/particle_filter/parameter_adjustment/narrow_std/anime_narrow_std.jl
@@ -0,0 +1,43 @@
+# module for checking narrow particle movement
+# using narrow noise parameters
+
+module AnimeNarrowStd
+  include(joinpath(split(@__FILE__, "src")[1], "src/model/world/world.jl"))
+  include(joinpath(split(@__FILE__, "src")[1], "src/model/robot/differential_wheeled_robot/differential_wheeled_robot.jl"))
+
+  function animate_per_time(time_interval, world, robot)
+    draw(world)
+    annotate!(-3.5, 4.5, "t = $(time_interval)", "black")
+    draw!(robot)
+  end
+
+  function main(time_interval=0.1; is_test=false)
+    w = World(-5.0, 5.0, -5.0, 5.0)
+
+    s = Sensor(Map(), dist_noise_rate=0.1, dir_noise=pi/90,
+               dist_bias_rate_stddev=0.1, dir_bias_stddev=pi/90)
+
+    init_pose = [0.0, 0.0, 0.0]
+    noises = Dict("nn"=>0.001, "no"=>0.002, "on"=>0.003, "oo"=>0.004)
+    e = MclRandSamp(init_pose, 100, noises)
+    circling = Agent(0.2, 10.0/180*pi, estimator=e)
+    r = DifferentialWheeledRobot(init_pose, 0.2, "black",
+                                 circling, time_interval,
+                                 noise_per_meter=5, 
+                                 noise_std=pi/30,
+                                 bias_rate_stds=[0.1, 0.1],
+                                 sensor=s)
+
+    if is_test == false
+      anime = @animate for t in 0:time_interval:30
+        animate_per_time(t, w, r)
+      end
+      path = "src/localization/particle_filter/parameter_adjustment/narrow_std/anime_narrow_std.gif"
+      gif(anime, fps=15, joinpath(split(@__FILE__, "src")[1], path))
+    else
+      for t in 0:time_interval:10
+        animate_per_time(t, w, r)
+      end
+    end
+  end
+end
--- test/test_localization.jl
+++ test/test_localization.jl
@@ -7,6 +7,8 @@ module TestLocalization
   include(joinpath(split(@__FILE__, "test")[1], "src/localization/particle_filter/particle/particle.jl"))
   include(joinpath(split(@__FILE__, "test")[1], "src/localization/particle_filter/random_sampling/anime_mcl_rand_samp.jl"))
   include(joinpath(split(@__FILE__, "test")[1], "src/localization/particle_filter/systematic_sampling/anime_mcl_sys_samp.jl"))
+  include(joinpath(split(@__FILE__, "test")[1], "src/localization/particle_filter/parameter_adjustment/narrow_std/anime_narrow_std.jl"))
+  include(joinpath(split(@__FILE__, "test")[1], "src/localization/particle_filter/parameter_adjustment/wide_std/anime_wide_std.jl"))
 
   function main()
     @testset "ParticleFilter" begin
@@ -22,6 +24,14 @@ module TestLocalization
       end
       @testset "SystematicSampling" begin
         @test_nowarn AnimeMclSysSamp.main(is_test=true)
+      end
+      @testset "ParameterAdjustment" begin
+        @testset "NarrowStd" begin
+          @test_nowarn AnimeNarrowStd.main(is_test=true)
+        end
+        @testset "WideStd" begin
+          @test_nowarn AnimeWideStd.main(is_test=true)
+        end
       end
     end
   end
