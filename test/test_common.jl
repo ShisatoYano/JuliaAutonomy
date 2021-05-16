c873466b54ed1e24c48b7073af2c4f36481608b3
Shisato <shisatoyano@gmail.com>
2021-05-16 22:49:52 +0000

    modified common dir

--- test/test_common.jl
+++ test/test_common.jl
@@ -1,13 +1,13 @@
-# module for testing each analysis modules
+# module for testing each common modules
 
-module TestAnalysis
+module TestCommon
   using Test
 
   # target modules
-  include(joinpath(split(@__FILE__, "test")[1], "src/analysis/error_calculation/error_calculation.jl"))
+  include(joinpath(split(@__FILE__, "test")[1], "src/common/error_calculation/error_calculation.jl"))
 
   function main()
-    @testset "Analysis" begin
+    @testset "Common" begin
       @testset "ErrorCalculation" begin
         error = calc_lon_lat_error([5.0, 5.0, 5.0], [0.0, 0.0, 0.0])
         @test error[1] == 5.00
