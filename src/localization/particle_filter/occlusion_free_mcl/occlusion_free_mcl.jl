d528474930df7b59625edd33995cf81df8343ad8
Shisato <shisatoyano@gmail.com>
2021-06-06 05:23:43 +0000

    implemented occlusion free mcl

---
+++ src/localization/particle_filter/occlusion_free_mcl/occlusion_free_mcl.jl
@@ -0,0 +1,85 @@
+# module for estimating pose by monte carlo localization
+# based on particle filter
+# particles are resampled by random sampling
+# considering occlusion
+
+using Distributions, LinearAlgebra, StatsBase
+
+include(joinpath(split(@__FILE__, "src")[1], "src/localization/particle_filter/occlusion_free_mcl/occlusion_free_particle.jl"))
+include(joinpath(split(@__FILE__, "src")[1], "src/model/map/map.jl"))
+
+mutable struct OcclusionFreeMcl
+  particles
+  motion_noise_rate_pdf
+  map
+  dist_dev
+  dir_dev
+  max_likelihood_particle
+  estimated_pose
+
+  # init
+  function OcclusionFreeMcl(init_pose::Array, num::Int64,
+                            motion_noise_stds::Dict;
+                            env_map=nothing,
+                            dist_dev_rate=0.0,
+                            dir_dev=0.0)
+    self = new()
+    self.particles = [OcclusionFreeParticle(init_pose, 1.0/num) for i in 1:num]
+    self.map = env_map
+    self.dist_dev = dist_dev_rate
+    self.dir_dev = dir_dev
+    self.max_likelihood_particle = self.particles[1]
+    self.estimated_pose = self.max_likelihood_particle.pose
+    
+    v = motion_noise_stds
+    c = diagm(0 => [v["nn"]^2, v["no"]^2, v["on"]^2, v["oo"]^2])
+    self.motion_noise_rate_pdf = MvNormal(c)
+    return self
+  end
+end
+
+function motion_update(self::OcclusionFreeMcl, speed, yaw_rate, time_interval)
+  for p in self.particles
+    motion_update(p, speed, yaw_rate, time_interval, self.motion_noise_rate_pdf)
+  end
+end
+
+function set_max_likelihood_pose(self::OcclusionFreeMcl)
+  max_index = argmax([p.weight for p in self.particles])
+  self.max_likelihood_particle = self.particles[max_index]
+  self.estimated_pose = self.max_likelihood_particle.pose
+end
+
+function observation_update(self::OcclusionFreeMcl, observation)
+  for p in self.particles
+    observation_update(p, observation, self.map, self.dist_dev, self.dir_dev)
+  end
+  set_max_likelihood_pose(self)
+  resampling(self)
+end
+
+function resampling(self::OcclusionFreeMcl)
+  ws = [(if p.weight <= 0.0 1e-100 else p.weight end) for p in self.particles]
+  
+  ps = sample(self.particles, Weights(ws), length(self.particles))
+  self.particles = [deepcopy(e) for e in ps]
+  for p in self.particles
+    p.weight = 1.0/length(self.particles)
+  end
+end
+
+function draw!(self::OcclusionFreeMcl)
+  k = 0.5 # scale for length of arrows
+  # all of particles
+  px = [p.pose[1] for p in self.particles]
+  py = [p.pose[2] for p in self.particles]
+  vx = [cos(p.pose[3])*k for p in self.particles]
+  vy = [sin(p.pose[3])*k for p in self.particles]
+  quiver!(px, py, quiver=(vx, vy), aspect_ratio=true, color="blue")
+  # maximum likelihood particle
+  mx = [self.estimated_pose[1]]
+  my = [self.estimated_pose[2]]
+  mvx = [cos(self.estimated_pose[3])*k]
+  mvy = [sin(self.estimated_pose[3])*k]
+  quiver!(mx, my, quiver=(mvx, mvy), aspect_ratio=true, color="red")
+end
--- src/localization/particle_filter/occlusion_free_mcl/occlusion_free_particle.jl
+++ src/localization/particle_filter/occlusion_free_mcl/occlusion_free_particle.jl
@@ -1,16 +1,17 @@
 # module for calculating particle motion
+# considering shorter distance observation by occlusion
 
 using Distributions, LinearAlgebra
 
 include(joinpath(split(@__FILE__, "src")[1], "src/common/state_transition/state_transition.jl"))
 include(joinpath(split(@__FILE__, "src")[1], "src/common/observation_function/observation_function.jl"))
 
-mutable struct Particle
+mutable struct OcclusionFreeParticle
   pose
   weight
 
   # init
-  function Particle(init_pose::Array, weight::Float64)
+  function OcclusionFreeParticle(init_pose::Array, weight::Float64)
     self = new()
     self.pose = init_pose
     self.weight = weight
@@ -18,16 +19,16 @@ mutable struct Particle
   end
 end
 
-function motion_update(self::Particle, speed, yaw_rate, 
-                       time_interval, noise_rate_pdf)
+function motion_update(self::OcclusionFreeParticle, speed, 
+                       yaw_rate, time_interval, noise_rate_pdf)
   ns = rand(noise_rate_pdf) # [nn, no, on, oo]
   noised_spd = speed + ns[1]*sqrt(abs(speed)/time_interval) + ns[2]*sqrt(abs(yaw_rate)/time_interval)
   noised_yr = yaw_rate + ns[3]*sqrt(abs(speed)/time_interval) + ns[4]*sqrt(abs(yaw_rate)/time_interval)
   self.pose = state_transition(noised_spd, noised_yr, time_interval, self.pose)
 end
 
-function observation_update(self::Particle, observation, env_map, 
-                            dist_dev_rate, dir_dev)
+function observation_update(self::OcclusionFreeParticle, observation, 
+                            env_map, dist_dev_rate, dir_dev)
   for obs in observation
     obs_pose = obs[1] # [distance, direction]
     obs_id = obs[2]
@@ -39,6 +40,9 @@ function observation_update(self::Particle, observation, env_map,
     # calculate likelihood
     dist_dev = dist_dev_rate * obs_from_particle[1]
     cov = diagm(0 => [dist_dev^2, dir_dev^2])
+    if obs_pose[1] < obs_from_particle[1]
+      obs_pose[1] = obs_from_particle[1]
+    end
     self.weight *= pdf(MvNormal(obs_from_particle, cov), obs_pose)
   end
 end
