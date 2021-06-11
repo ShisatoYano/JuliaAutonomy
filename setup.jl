23f6f065e26c9865ea23d96d2c47b50a0153a769
Shisato <shisatoyano@gmail.com>
2021-06-11 08:14:42 +0000

    install PDMats package

--- setup.jl
+++ setup.jl
@@ -8,7 +8,7 @@ module Setup
         :Plots, :DataFrames, :CSV, :Test,
         :StatsPlots, :Statistics, :FreqTables, 
         :NamedArrays, :Distributions, :LinearAlgebra,
-        :StatsBase
+        :StatsBase, :PDMats
     ]
 
     function install_packages()
