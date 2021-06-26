6791b9ca0c63077a975c08fedcfbfda78a46fc49
Shisato <shisatoyano@gmail.com>
2021-06-26 13:54:59 +0000

    implement calculate orientation

--- src/slam/graph_based_slam/psi_sensor.jl
+++ src/slam/graph_based_slam/psi_sensor.jl
@@ -6,7 +6,7 @@ pyplot()
 include(joinpath(split(@__FILE__, "src")[1], "src/model/map/map.jl"))
 include(joinpath(split(@__FILE__, "src")[1], "src/common/observation_function/observation_function.jl"))
 
-mutable struct Sensor
+mutable struct PsiSensor
   map
   last_data
   dist_rng
@@ -22,18 +22,18 @@ mutable struct Sensor
   occlusion_prob
 
   # init
-  function Sensor(map::Map;
-                  dist_rng::Tuple=(0.5, 6.0), 
-                  dir_rng::Tuple=(-pi/3, pi/3),
-                  dist_noise_rate=0.0,
-                  dir_noise=0.0,
-                  dist_bias_rate_stddev=0.0,
-                  dir_bias_stddev=0.0,
-                  phantom_prob=0.0,
-                  phantom_rng_x::Tuple=(-5.0, 5.0),
-                  phantom_rng_y::Tuple=(-5.0, 5.0),
-                  oversight_prob=0.0,
-                  occlusion_prob=0.0)
+  function PsiSensor(map::Map;
+                     dist_rng::Tuple=(0.5, 6.0), 
+                     dir_rng::Tuple=(-pi/3, pi/3),
+                     dist_noise_rate=0.0,
+                     dir_noise=0.0,
+                     dist_bias_rate_stddev=0.0,
+                     dir_bias_stddev=0.0,
+                     phantom_prob=0.0,
+                     phantom_rng_x::Tuple=(-5.0, 5.0),
+                     phantom_rng_y::Tuple=(-5.0, 5.0),
+                     oversight_prob=0.0,
+                     occlusion_prob=0.0)
     self = new()
     self.map = map
     self.last_data = []
@@ -54,7 +54,7 @@ mutable struct Sensor
   end
 end
 
-function visible(self::Sensor, obsrv=nothing)
+function visible(self::PsiSensor, obsrv=nothing)
   if obsrv === nothing
     return false
   end
@@ -63,17 +63,17 @@ function visible(self::Sensor, obsrv=nothing)
     && (self.dir_rng[1] <= obsrv[2] <= self.dir_rng[2]))
 end
 
-function noise(self::Sensor, obsrv::Array)
+function noise(self::PsiSensor, obsrv::Array)
   ell = rand(Normal(obsrv[1], obsrv[1] * self.dist_noise_rate))
   phi = rand(Normal(obsrv[2], self.dir_noise))
   return [ell, phi]
 end
 
-function bias(self::Sensor, obsrv::Array)
+function bias(self::PsiSensor, obsrv::Array)
   return obsrv + [obsrv[1] * self.dist_bias_rate_std, self.dir_bias]
 end
 
-function phantom(self::Sensor, sns_pose::Array, obsrv::Array)
+function phantom(self::PsiSensor, sns_pose::Array, obsrv::Array)
   if rand(Uniform()) < self.phantom_prob
     phantom_pose = [rand(self.phantom_dist_x), rand(self.phantom_dist_y)]
     return observation_function(sns_pose, phantom_pose)
@@ -82,7 +82,7 @@ function phantom(self::Sensor, sns_pose::Array, obsrv::Array)
   end
 end
 
-function oversight(self::Sensor, obsrv::Array)
+function oversight(self::PsiSensor, obsrv::Array)
   if rand(Uniform()) < self.oversight_prob
     return nothing
   else
@@ -90,7 +90,7 @@ function oversight(self::Sensor, obsrv::Array)
   end
 end
 
-function occlusion(self::Sensor, obsrv::Array)
+function occlusion(self::PsiSensor, obsrv::Array)
   if rand(Uniform()) < self.occlusion_prob
     ell = obsrv[1] - rand(Uniform()) * (self.dist_rng[2] - obsrv[1])
     phi = obsrv[2]
@@ -100,23 +100,31 @@ function occlusion(self::Sensor, obsrv::Array)
   end
 end
 
-function data(self::Sensor, sns_pose::Array)
+function data(self::PsiSensor, sns_pose::Array; orient_noise=pi/90)
   observed = []
   for obj in self.map.objects
+    # calculate orientation
+    diff_x = sns_pose[1] - obj.pose[1]
+    diff_y = sns_pose[2] - obj.pose[2]
+    psi = rand(MvNormal(atan(diff_y, diff_x), orient_noise))
+
+    # calculate observation
     obsrv = observation_function(sns_pose, obj.pose)
+    
+    # add disturbance
     obsrv = phantom(self, sns_pose, obsrv)
     obsrv = occlusion(self, obsrv)
     obsrv = oversight(self, obsrv)
     if visible(self, obsrv)
       obsrv = bias(self, obsrv)
       obsrv = noise(self, obsrv)
-      push!(observed, (obsrv, obj.id))
+      push!(observed, ([obsrv[1], obsrv[2], psi], obj.id))
     end
   end
   self.last_data = observed
 end
 
-function draw!(self::Sensor, sns_pose::Array)
+function draw!(self::PsiSensor, sns_pose::Array)
   for obj in self.last_data
     x, y, theta = sns_pose[1], sns_pose[2], sns_pose[3]
     distance, direction = obj[1][1], obj[1][2]
