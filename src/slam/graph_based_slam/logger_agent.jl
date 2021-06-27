f444443ec765ca0da00b205286388885770a5ec1
Shisato <shisatoyano@gmail.com>
2021-06-27 12:26:08 +0000

    implement writing log file

--- src/slam/graph_based_slam/logger_agent.jl
+++ src/slam/graph_based_slam/logger_agent.jl
@@ -2,6 +2,8 @@
 # decide control order to robot
 # log pose and observation into text file
 
+include(joinpath(split(@__FILE__, "src")[1], "src/common/state_transition/state_transition.jl"))
+
 mutable struct LoggerAgent
   speed
   yaw_rate
@@ -27,16 +29,33 @@ mutable struct LoggerAgent
       self.prev_yr = 0.0
       self.pose = init_pose
       self.step = 0
+      self.log = open("log.txt", "w")
       return self
   end
 end
 
-function draw_decision!(self::Agent, observation)
-  if self.estimator !== nothing
-      motion_update(self.estimator, self.prev_spd, self.prev_yr, self.time_interval)
-      self.prev_spd, self.prev_yr = self.speed, self.yaw_rate
-      observation_update(self.estimator, observation)
-      draw!(self.estimator)
+function close_log_file(self::LoggerAgent)
+  close(self.log)
+end
+
+function draw_decision!(self::LoggerAgent, observation)
+  # logging trajectory and observation
+  if length(observation) > 0
+    write(self.log, "x $(self.step) $(self.pose[1]) $(self.pose[2]) $(self.pose[3])") # step x y theta
+    for obs in observation
+      write(self.log, "z $(self.step) $(obs[2]) $(obs[1][1]) $(obs[1][2]) $(obs[1][3])") # step id distance direction orientation
+    end
+    self.step += 1
   end
+
+  # draw estimated pose
+  if self.estimator !== nothing
+    motion_update(self.estimator, self.prev_spd, self.prev_yr, self.time_interval)
+    self.prev_spd, self.prev_yr = self.speed, self.yaw_rate
+    observation_update(self.estimator, observation)
+    draw!(self.estimator)
+  end
+
+  self.pose = state_transition(self.speed, self.yaw_rate, self.time_interval, self.pose)
   return self.speed, self.yaw_rate
 end
