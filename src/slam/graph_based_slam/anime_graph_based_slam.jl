cabc69e391472935cf954763545346206a26f996
Shisato <shisatoyano@gmail.com>
2021-06-29 13:59:34 +0000

    draw saved trajectory and observation as png

--- src/slam/graph_based_slam/anime_graph_based_slam.jl
+++ src/slam/graph_based_slam/anime_graph_based_slam.jl
@@ -35,13 +35,43 @@ module AnimeGraphBasedSlam
          xlims=(-5.0, 5.0), ylims=(-5.0, 5.0), legend=false)
   end
 
-  function draw(pose_list, obsrv_list)
-    make_axis()
+  function draw_observation(pose_list, obsrv_list)
+    for step in 0:length(pose_list)-1
+      step_str = string(step)
+      if haskey(obsrv_list, step_str)
+        for obsrv in obsrv_list[step_str]
+          x, y, theta = pose_list[step_str][1], pose_list[step_str][2], pose_list[step_str][3]
+          dist, dir = obsrv[2][1], obsrv[2][2]
+          mx = x + dist * cos(theta + dir)
+          my = y + dist * sin(theta + dir)
+          plot!([x, mx], [y, my], color="pink", legend=false,
+                aspect_ratio=true, alpha=0.5)
+        end
+      end
+    end
   end
 
-  function main()
+  function draw_trajectory(pose_list)
+    x_list = [pose_list[string(step)][1] for step in 0:length(pose_list)-1]
+    y_list = [pose_list[string(step)][2] for step in 0:length(pose_list)-1]
+    scatter!(x_list, y_list, markersize=2, markercolor="black")
+    plot!(x_list, y_list, linewidth=0.5, color="black")
+  end
+
+  function draw(pose_list, obsrv_list)
+    make_axis()
+    draw_observation(pose_list, obsrv_list)
+    draw_trajectory(pose_list)
+  end
+
+  function main(is_test=false)
     pose_list, obsrv_list = read_data()
     
     draw(pose_list, obsrv_list)
+
+    if is_test == false
+      save_path = joinpath(split(@__FILE__, "src")[1], "src/slam/graph_based_slam/traj_obsrv_log.png")
+      savefig(save_path)
+    end
   end
 end
