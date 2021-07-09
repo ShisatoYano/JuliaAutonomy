a5158d0165e886e5c890680397b7c236a9996e88
Shisato <shisatoyano@gmail.com>
2021-07-09 02:29:58 +0000

    add graph based slam test code

--- test/test_slam.jl
+++ test/test_slam.jl
@@ -8,6 +8,8 @@ module TestSlam
   include(joinpath(split(@__FILE__, "test")[1], "src/slam/map_particle.jl"))
   include(joinpath(split(@__FILE__, "test")[1], "src/slam/fast_slam_1/anime_fast_slam_1.jl"))
   include(joinpath(split(@__FILE__, "test")[1], "src/slam/fast_slam_2/anime_fast_slam_2.jl"))
+  include(joinpath(split(@__FILE__, "test")[1], "src/slam/graph_based_slam/traj_obsrv_logger.jl"))
+  include(joinpath(split(@__FILE__, "test")[1], "src/slam/graph_based_slam/anime_graph_based_slam.jl"))  
 
   function main()
     @testset "Slam" begin
@@ -33,6 +35,9 @@ module TestSlam
       end
       @testset "FastSlam2.0" begin
         @test_nowarn AnimeFastSlam2.main(is_test=true)
+      end
+      @testset "GraphBasedSlam" begin
+        @test_nowarn AnimeGraphBasedSlam.main(true)
       end
     end
   end
