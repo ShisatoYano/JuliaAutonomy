1114858ae6d29f5e1a1af7c0a4a0073a4d9dd7ad
Shisato <shisatoyano@gmail.com>
2021-07-09 06:40:35 +0000

    add psi sensor test

--- test/test_slam.jl
+++ test/test_slam.jl
@@ -8,6 +8,7 @@ module TestSlam
   include(joinpath(split(@__FILE__, "test")[1], "src/slam/map_particle.jl"))
   include(joinpath(split(@__FILE__, "test")[1], "src/slam/fast_slam_1/anime_fast_slam_1.jl"))
   include(joinpath(split(@__FILE__, "test")[1], "src/slam/fast_slam_2/anime_fast_slam_2.jl"))
+  include(joinpath(split(@__FILE__, "test")[1], "src/slam/graph_based_slam/psi_sensor.jl"))
   include(joinpath(split(@__FILE__, "test")[1], "src/slam/graph_based_slam/traj_obsrv_logger.jl"))
   include(joinpath(split(@__FILE__, "test")[1], "src/slam/graph_based_slam/anime_graph_based_slam.jl"))  
 
@@ -35,6 +36,18 @@ module TestSlam
       end
       @testset "FastSlam2.0" begin
         @test_nowarn AnimeFastSlam2.main(is_test=true)
+      end
+      @testset "PsiSensor" begin
+        m = Map()
+        add_object(m, Object(2.0, -2.0))
+        s = PsiSensor(m)
+        @test s.last_data == []
+        @test s.dist_rng == (0.5, 6.0)
+        @test s.dir_rng == (-pi/3, pi/3)
+        @test visible(s, [0.5, -pi/3]) == true
+        @test visible(s, [6.0, pi/3]) == true
+        @test visible(s, [0.2, -pi/3]) == false
+        @test visible(s, [0.5, pi]) == false
       end
       @testset "GraphBasedSlam" begin
         @test_nowarn AnimeGraphBasedSlam.main(true)
