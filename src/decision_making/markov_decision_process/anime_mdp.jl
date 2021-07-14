4fdb1522f2bb9cd8e3bce0cb26a335c6e222904c
Shisato <shisatoyano@gmail.com>
2021-07-14 14:35:35 +0000

    implementing puddle world animation

--- src/decision_making/markov_decision_process/anime_mdp.jl
+++ src/decision_making/markov_decision_process/anime_mdp.jl
@@ -2,59 +2,52 @@
 # problem as markov decision precess
 
 module AnimeMdp
-  include(joinpath(split(@__FILE__, "src")[1], "src/model/world/world.jl"))
-  include(joinpath(split(@__FILE__, "src")[1], "src/model/robot/differential_wheeled_robot/differential_wheeled_robot.jl"))
-  include(joinpath(split(@__FILE__, "src")[1], "src/model/goal/goal.jl"))
-  include(joinpath(split(@__FILE__, "src")[1], "src/localization/extended_kalman_filter/extended_kalman_filter.jl"))
-  include(joinpath(split(@__FILE__, "src")[1], "src/model/puddle/puddle.jl"))
+  include(joinpath(split(@__FILE__, "src")[1], "src/model/world/puddle_world.jl"))
+  include(joinpath(split(@__FILE__, "src")[1], "src/model/map/map.jl"))
+  # include(joinpath(split(@__FILE__, "src")[1], "src/model/robot/differential_wheeled_robot/differential_wheeled_robot.jl"))
+  # include(joinpath(split(@__FILE__, "src")[1], "src/model/goal/goal.jl"))
+  # include(joinpath(split(@__FILE__, "src")[1], "src/localization/extended_kalman_filter/extended_kalman_filter.jl"))
+  # include(joinpath(split(@__FILE__, "src")[1], "src/model/puddle/puddle.jl"))
 
-  function animate_per_time(time_interval, world, map, robot, goal, puddle1, puddle2)
-    draw(world)
-    annotate!(-3.5, 4.5, "t = $(time_interval)", "black")
-    draw!(map)
-    draw!(robot)
-    draw!(goal)
-    draw!(puddle1)
-    draw!(puddle2)
-  end
+  function main(delta_time=0.1, end_time=30; is_test=false)
+    # save path of gif file
+    path = "src/decision_making/markov_decision_process/anime_mdp.gif"
 
-  function main(time_interval=0.1; is_test=false)
-    w = World(-5.0, 5.0, -5.0, 5.0)
-
-    m = Map()
+    # simulation world
+    world = PuddleWorld(-5.0, 5.0, -5.0, 5.0,
+                        delta_time=delta_time,
+                        end_time=end_time,
+                        is_test=is_test,
+                        save_path=path)
+    
+    # map including landmarks 
+    map = Map()
     landmark_positions = [[-4.0, 2.0],[2.0, -3.0],[3.0, 3.0]]
     for (idx, pos) in enumerate(landmark_positions)
-      add_object(m, Object(pos[1], pos[2], id=idx))
+      add_object(map, Object(pos[1], pos[2], id=idx))
     end
-    g = Goal(-3.0, -3.0)
+    append(world, map)
 
-    s = Sensor(m, dist_noise_rate=0.1, dir_noise=pi/90)
+    # robot including sensor, agent, estimator
     
-    init_pose = [0.0, 0.0, 0.0]
-    ekf = ExtendedKalmanFilter(init_pose, env_map=m,
-                               dist_dev_rate=0.14,
-                               dir_dev=0.05)
-    a = Agent(0.2, 10.0/180*pi, estimator=ekf)
-    r = DifferentialWheeledRobot(init_pose, 0.2, "red",
-                                 a, time_interval,
-                                 noise_per_meter=5, 
-                                 noise_std=pi/30,
-                                 bias_rate_stds=[0.1, 0.1],
-                                 sensor=s)
+    draw(world)
+  #   g = Goal(-3.0, -3.0)
+
+  #   s = Sensor(m, dist_noise_rate=0.1, dir_noise=pi/90)
+    
+  #   init_pose = [0.0, 0.0, 0.0]
+  #   ekf = ExtendedKalmanFilter(init_pose, env_map=m,
+  #                              dist_dev_rate=0.14,
+  #                              dir_dev=0.05)
+  #   a = Agent(0.2, 10.0/180*pi, estimator=ekf)
+  #   r = DifferentialWheeledRobot(init_pose, 0.2, "red",
+  #                                a, time_interval,
+  #                                noise_per_meter=5, 
+  #                                noise_std=pi/30,
+  #                                bias_rate_stds=[0.1, 0.1],
+  #                                sensor=s)
   
-    p1 = Puddle([-2.0, 0.0], [0.0, 2.0], 0.1)
-    p2 = Puddle([-0.5, -2.0], [2.5, 1.0], 0.1)
-    
-    if is_test == false
-      anime = @animate for t in 0:time_interval:30
-        animate_per_time(t, w, m, r, g, p1, p2)
-      end
-      path = "src/decision_making/markov_decision_process/anime_mdp.gif"
-      gif(anime, fps=15, joinpath(split(@__FILE__, "src")[1], path))
-    else
-      for t in 0:time_interval:10
-        animate_per_time(t, w, m, r, g)
-      end
-    end
+  #   p1 = Puddle([-2.0, 0.0], [0.0, 2.0], 0.1)
+  #   p2 = Puddle([-0.5, -2.0], [2.5, 1.0], 0.1)
   end
 end
--- src/model/world/puddle_world.jl
+++ src/model/world/puddle_world.jl
@@ -1,30 +1,62 @@
 # module for defining world coordinate system
+# puddle can be put
 
 using Plots
 pyplot()
 
-mutable struct World
-    x_min
-    x_max
-    y_min
-    y_max
+mutable struct PuddleWorld
+  x_min
+  x_max
+  y_min
+  y_max
+  objects
+  delta_time
+  end_time
+  is_test
+  save_path
 
-    # init
-    function World(x_min::Float64, x_max::Float64,
-                   y_min::Float64, y_max::Float64)
-        self = new()
-        self.x_min = x_min
-        self.x_max = x_max
-        self.y_min = y_min
-        self.y_max = y_max
-        return self
-    end
+  # init
+  function PuddleWorld(x_min::Float64, x_max::Float64,
+                       y_min::Float64, y_max::Float64;
+                       delta_time=0.1, end_time=30,
+                       is_test=false, save_path=nothing)
+    self = new()
+    self.x_min = x_min
+    self.x_max = x_max
+    self.y_min = y_min
+    self.y_max = y_max
+    self.objects = []
+    self.delta_time = delta_time
+    self.end_time = end_time
+    self.is_test = is_test
+    self.save_path = save_path
+    return self
+  end
 end
 
-function draw(self::World)
-    plot([], [], aspect_ratio=true,
-         xlabel="X", ylabel="Y",
-         xlims=(self.x_min, self.x_max),
-         ylims=(self.y_min, self.y_max),
-         legend=false)
+function append(self::PuddleWorld, obj)
+  push!(self.objects, obj)
+end
+
+function one_step(self::PuddleWorld, delta_time)
+  plot([], [], aspect_ratio=true, xlabel="X", ylabel="Y",
+       xlims=(self.x_min, self.x_max), ylims=(self.y_min, self.y_max),
+       legend=false)
+  annotate!(-3.5, 4.5, "t = $(delta_time)", "black")
+  for obj in self.objects
+    draw!(obj)
+  end
+end
+
+function draw(self::PuddleWorld) 
+  if self.is_test
+    for t in 0:self.delta_time:10
+      one_step(self, t)
+    end
+  else
+    anime = @animate for t in 0:self.delta_time:self.end_time
+      one_step(self, t)
+    end
+    gif(anime, fps=15, joinpath(split(@__FILE__, "src")[1], self.save_path))
+  end
 end
