aa50fc69cd1f6e028e01a9dc6419d2f7e1ab6b8b
Shisato <shisatoyano@gmail.com>
2021-07-29 14:22:02 +0000

    add epsilon greedy

--- src/decision_making/reinforcement_learning/q_learning/state_info.jl
+++ src/decision_making/reinforcement_learning/q_learning/state_info.jl
@@ -1,13 +1,18 @@
 # module for storing q value at each state
 
+using Random
+using StatsBase
+
 mutable struct StateInfo
   q
+  epsilon
 
   # init
-  function StateInfo(action_num=0)
+  function StateInfo(action_num=0; epsilon=0.3)
     self = new()
 
     self.q = zeros(action_num)
+    self.epsilon = epsilon
 
     return self
   end
@@ -19,7 +24,16 @@ function greedy(self::StateInfo)
   return argmax(self.q)
 end
 
+# select another action following probability epsilon
+function epsilon_greedy(self::StateInfo)
+  if rand(1)[1] < self.epsilon
+    return rand(1:length(self.q), 1)[1]
+  else
+    return greedy(self)
+  end
+end
+
 # call greedy()
 function _pi(self::StateInfo)
-  return greedy(self)
+  return epsilon_greedy(self)
 end
