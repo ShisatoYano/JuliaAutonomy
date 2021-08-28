122b91acc6c3ac4e38dc5372c066687d852e6d54
Shisato <shisatoyano@gmail.com>
2021-08-28 07:50:05 +0000

    implemented variation inference sample codes

--- src/variational_inference/variational_inference.jl
+++ src/variational_inference/variational_inference.jl
@@ -11,11 +11,11 @@ module VariationalInference
     all_data = CSV.read(data_path, DataFrame, 
                         header=["date", "time", "ir", "z"],
                         delim=' ')
-    data = sort(sample(all_data.z, 1000))
+    data = sort(sample(all_data.z, 3000))
     data = DataFrame(z=data)
     
     # initialize responsibility
-    K = 2 # num of class
+    K = 3 # num of class
     n = Int(ceil(size(data)[1]/K))
     for k in 1:K
       data_k = []
@@ -26,34 +26,47 @@ module VariationalInference
           append!(data_k, 0.0)
         end
       end
+
       if k == 1
         data = hcat(data, DataFrame(one=data_k))
-      else  
+      elseif k == 2
         data = hcat(data, DataFrame(two=data_k))
+      else  
+        data = hcat(data, DataFrame(three=data_k))
       end
     end
 
+    # iteration
+    params_history = Dict()
     params = DataFrame()
-    for k in 1:K
-      if k == 1
-        params = update_parameters(data, k)
-      else
-        params = vcat(params, update_parameters(data, k))
+    for t in 1:10000
+      data, params = one_step(data, K)
+      # record per 10 times
+      if t % 10 == 0
+        params_history[t] = params
+        println("$(t) $(params)")
+
+        if length(params_history) < 2
+          continue
+        end
+        
+        if all([(abs(params_history[t-10][k, "tau"] - params_history[t][k, "tau"]) < 10e-5) for k in 1:K])
+          break
+        end
       end
     end
     draw_sensor_dist(data, params, K,
-                     "src/variational_inference/draw_sensor_dist_sample.png",
+                     "src/variational_inference/draw_sensor_dist_K3_t340.png",
                      test)
-    
-    rs = [responsibility(data[i, :z], K, params) for i in 1:size(data)[1]]
-    println(rs)
   end
 
   function update_parameters(ds, k, mu_avg=600, zeta=1, alpha=1, beta=1, tau=1)
     if k == 1
       key_class = "one"
+    elseif k == 2
+      key_class = "two"
     else
-      key_class = "two" 
+      key_class = "three" 
     end
 
     # calculate R, S, T
@@ -100,14 +113,15 @@ module VariationalInference
     
     r = Dict()
     for k in 1:K
-      log_rho = (digamma(params[k, "alpha"]) - log(params[k, "beta"]))/2 \
-                - (1/params[k, "zeta"] + ((params[k, "mu_avg"]-z)^2)*params[k, "alpha"]/params[k, "beta"])/2 \
-                + digamma(params[k, "tau"]) - digamma(tau_sum)
+      log_rho = (digamma(params[k, "alpha"]) - log(params[k, "beta"]))/2 - (1/params[k, "zeta"] + ((params[k, "mu_avg"]-z)^2)*params[k, "alpha"]/params[k, "beta"])/2 + digamma(params[k, "tau"]) - digamma(tau_sum)
       if k == 1
         r["one"] = exp(log_rho)
-      else
+      elseif k == 2
         r["two"] = exp(log_rho)
+      else
+        r["three"] = exp(log_rho)
       end
+      
     end
 
     # sum of r_k
@@ -115,8 +129,10 @@ module VariationalInference
     for k in 1:K
       if k == 1
         append!(rks, r["one"])
+      elseif k == 2
+        append!(rks, r["two"])
       else
-        append!(rks, r["two"])  
+        append!(rks, r["three"])
       end
     end
     w = sum(rks)
@@ -125,12 +141,40 @@ module VariationalInference
     for k in 1:K
       if k == 1
         r["one"] /= w
-      else
+      elseif k == 2
         r["two"] /= w
+      else
+        r["three"] /= w
       end
     end
 
     return r
+  end
+
+  function one_step(data, K)
+    # update parameters
+    params = DataFrame()
+    for k in 1:K
+      if k == 1
+        params = update_parameters(data, k)
+      else
+        params = vcat(params, update_parameters(data, k))
+      end
+    end
+
+    # update responsibility
+    rs = [responsibility(data[i, :z], K, params) for i in 1:size(data)[1]]
+    for k in 1:K
+      if k == 1
+        data.one = [rs[i]["one"] for i in 1:size(data)[1]]
+      elseif k == 2
+        data.two = [rs[i]["two"] for i in 1:size(data)[1]]
+      else
+        data.three = [rs[i]["three"] for i in 1:size(data)[1]]
+      end
+    end
+
+    return data, params
   end
 
 end
